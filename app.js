7c9d82d6a8ea4e6e65851f5f3d2cfe607a8f9575
John Foley <john@hisfoleyness.com>
2015-02-11 10:26:37 -0800

    add lizard and spock

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ function determineWinner(player1Choice, player2Choice) {
     { weapons: ['rock', 'paper'], winner: 'paper', message: 'paper wraps rock' },
     { weapons: ['rock', 'scissors'], winner: 'rock', message: 'rock smashes scissors' },
     { weapons: ['paper', 'scissors'], winner: 'scissors', message: 'scissors cut paper' },
+    { weapons: ['rock', 'lizard'], winner: 'rock', message: 'rock smashes lizard' },
+    { weapons: ['rock', 'spock'], winner: 'spock', message: 'spock vaporizes rock' },
+    { weapons: ['lizard', 'paper'], winner: 'lizard', message: 'lizard eats paper' },
+    { weapons: ['lizard', 'scissors'], winner: 'scissors', message: 'scissors decapitate lizard' },
+    { weapons: ['spock', 'scissors'], winner: 'spock', message: 'spock smashes scissors' },
+    { weapons: ['spock', 'paper'], winner: 'paper', message: 'paper disproves spock' },
+    { weapons: ['spock', 'lizard'], winner: 'lizard', message: 'lizard poisons spock' },
   ]
 
   var rule = _.find(rules, function(rule) {
     return _.difference(rule.weapons, [player1Choice, player2Choice]).length == 0;
   });
+
+  if(rule === undefined) {
+    console.log({player1: player1Choice, player2: player2Choice})
+    throw {player1: player1Choice, player2: player2Choice}
+  }
 
   var winner = player1Choice === rule.winner ? 'player1' : 'player2';
 
@@ -22,7 +34,7 @@ function determineWinner(player1Choice, player2Choice) {
 }
 
 function playMatch(playerChoice) {
-  var weapons = ['rock', 'paper', 'scissors'];
+  var weapons = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
   var computerChoice = _.sample(weapons);
   $('#computerChoice').text(computerChoice);
@@ -42,8 +54,6 @@ function playMatch(playerChoice) {
 }
 
 $(function() {
-
-
   $('#rock').on('click', function() {
     playMatch('rock');
   });
@@ -56,5 +66,13 @@ $(function() {
 
   $('#scissors').on('click', function() {
     playMatch('scissors');
+  });
+
+  $('#lizard').on('click', function() {
+    playMatch('lizard');
+  });
+
+  $('#spock').on('click', function() {
+    playMatch('spock');
   });
 });
