121b5c3655c89a649d1704c02db3e74e72f1c0b4
John Foley <john@hisfoleyness.com>
2015-02-11 10:14:44 -0800

    more generic solution

--- app.js
+++ app.js
@@ -1,49 +1,60 @@
-$(function() {
+function determineWinner(player1Choice, player2Choice) {
+  if(player1Choice === player2Choice) {
+    return {winner: null, message: 'Its a draw'};
+  }
+
+  var rules = [
+    { weapons: ['rock', 'paper'], winner: 'paper', message: 'paper wraps rock' },
+    { weapons: ['rock', 'scissors'], winner: 'rock', message: 'rock smashes scissors' },
+    { weapons: ['paper', 'scissors'], winner: 'scissors', message: 'scissors cut paper' },
+  ]
+
+  var rule = _.find(rules, function(rule) {
+    return _.difference(rule.weapons, [player1Choice, player2Choice]).length == 0;
+  });
+
+  var winner = player1Choice === rule.winner ? 'player1' : 'player2';
+
+  return {
+    winner: winner,
+    message: rule.message
+  };
+}
+
+function playMatch(playerChoice) {
   var weapons = ['rock', 'paper', 'scissors'];
 
-  $('#rock').on('click', function() {
-    var computerChoice = _.sample(weapons);
-    $('#computerChoice').text(computerChoice);
+  var computerChoice = _.sample(weapons);
+  $('#computerChoice').text(computerChoice);
 
-    var message = $('#message');
-    if(computerChoice === 'rock') {
-      message.text('Its a draw');
-    } else if(computerChoice === 'scissors') {
-      message.text('Rock smashes scissors, you win!');
-    } else {
-      message.text('Paper wraps rock, you lose');
-    }
+  var result = determineWinner(playerChoice, computerChoice);
+
+  var messageNode = $('#message');
+  var message = result.message;
+
+  if(result.winner == 'player1') {
+    message += ', you win!';
+  } else if(result.winner == 'player2') {
+    message += ', you lose.';
+  }
+
+  messageNode.text(message);
+}
+
+$(function() {
+
+
+  $('#rock').on('click', function() {
+    playMatch('rock');
   });
 
 
   $('#paper').on('click', function() {
-    var computerChoice = _.sample(weapons);
-    $('#computerChoice').text(computerChoice);
-
-    var message = $('#message');
-    if(computerChoice === 'rock') {
-      message.text('Paper wraps rock, you win!');
-    } else if(computerChoice === 'scissors') {
-      message.text('Scissors cut paper, you lose');
-    } else {
-      message.text('Its a draw');
-    }
+    playMatch('paper');
   });
 
 
   $('#scissors').on('click', function() {
-    var computerChoice = _.sample(weapons);
-    $('#computerChoice').text(computerChoice);
-
-    var message = $('#message');
-    if(computerChoice === 'rock') {
-      message.text('Rock smashes scissors, you lose');
-    } else if(computerChoice === 'scissors') {
-      message.text('Its a draw');
-    } else {
-      message.text('Scissors cut paper, you win!');
-    }
+    playMatch('scissors');
   });
-
-
 });
